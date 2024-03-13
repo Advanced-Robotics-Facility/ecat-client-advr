19ff32556b8cd11d09721b1bacf1fb6a8aab28d7
DavideAntonucci <davide.antonucci@iit.it>
2024-03-13 15:02:18 +0100

    Added setup for a generic project.

---
+++ docs/scripts/setup.bash
@@ -0,0 +1,5 @@
+# setup environment
+export LD_LIBRARY_PATH=/home/embedded/dls_ws/install/lib:$LD_LIBRARY_PATH
+export CMAKE_PREFIX_PATH=/home/embedded/dls_ws/install:$CMAKE_PREFIX_PATH
+export PATH=/home/embedded/dls_ws/install/bin:$PATH
+export PKG_CONFIG_PATH=/home/embedded/dls_ws/install/lib/pkgconfig:$PKG_CONFIG_PATH
