1600fe2dfb97cb28b67cd013506f952e9ce46258
DavideAntonucci <davide.antonucci@iit.it>
2025-02-13 14:20:00 +0100

    Added linux signal to close correctly tthe GUI.

--- src/gui/ec_gui/src/ec_gui_main.cpp
+++ src/gui/ec_gui/src/ec_gui_main.cpp
@@ -8,9 +8,31 @@
 
 #include "utils/ec_utils.h"
 
+void catchUnixSignals(std::initializer_list<int> quitSignals) {
+    auto handler = [](int sig) -> void {
+        // blocking and not aysnc-signal-safe func are valid
+        printf("\nquit the application by signal(%d).\n", sig);
+        QCoreApplication::quit();
+    };
+    
+    sigset_t blocking_mask;   
+    sigemptyset(&blocking_mask);  
+    for (auto sig : quitSignals) 
+        sigaddset(&blocking_mask, sig);  
+        
+    struct sigaction sa;   
+    sa.sa_handler = handler;   
+    sa.sa_mask    = blocking_mask;  
+    sa.sa_flags   = 0;    
+    
+    for (auto sig : quitSignals)   
+        sigaction(sig, &sa, nullptr);
+}
+
 int main(int argc, char *argv[])
 {
     QCoreApplication::setAttribute(Qt::AA_ShareOpenGLContexts);
+    catchUnixSignals({SIGQUIT, SIGINT, SIGTERM, SIGHUP});
     QApplication a(argc, argv);
 
     QMainWindow mw;
