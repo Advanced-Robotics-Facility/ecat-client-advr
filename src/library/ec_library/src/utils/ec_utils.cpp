7c1809b340c2028937b1e224040efab6e1d4bd8d
DavideAntonucci <davide.antonucci@iit.it>
2025-06-20 12:56:56 +0200

    Reviewed utils class.

--- src/library/ec_library/src/utils/ec_utils.cpp
+++ src/library/ec_library/src/utils/ec_utils.cpp
@@ -59,6 +59,9 @@ EcUtils::EcUtils()
         _ec_cfg.period_ms=100;
     }else{
         _ec_cfg.period_ms=ec_cfg_node["general"]["period_ms"].as<int>();
+        if(_ec_cfg.period_ms==0){
+            throw std::runtime_error("Period cannot be zero!");
+        }
     }
     
     if(!ec_cfg_node["general"]["logging"]){
@@ -139,7 +142,7 @@ void EcUtils::compute_absolute_path(std::string dir_path,std::string &file_path)
     file_path=result;
 }
 
-void EcUtils::device_config_map(const YAML::Node & device_config_node)
+void EcUtils::device_config_map(const YAML::Node & device_config_node,std::string device_type)
 {
     auto robot_id_map=_robot_id_map_node["joint_map"].as<map<int,std::string>>(); // for xbotinterface
     for ( const auto &[esc_id, esc_name] : robot_id_map){
@@ -165,16 +168,28 @@ void EcUtils::device_config_map(const YAML::Node & device_config_node)
                         throw std::runtime_error("Error invalid dimension of gains. It has to be equal to five! ");
                 }
                 
-                if(device_config_node[esc_name]["motor_type"]){
-                    std::string type_str=device_config_node[esc_name]["motor_type"].as<std::string>();
-                    
-                    if(type_str=="ADVRF"){
-                        _ec_cfg.device_config_map[esc_id].type=iit::ecat::CENT_AC;
-                    }else if(type_str=="Synapticon"){
-                        _ec_cfg.device_config_map[esc_id].type=iit::ecat::SYNAPTICON_v5_1;
-                    }else{
-                        _ec_cfg.device_config_map[esc_id].type=iit::ecat::CENT_AC;
+                _ec_cfg.device_config_map[esc_id].type=0;
+                if(device_type=="motor"){
+                    if(device_config_node[esc_name]["motor_type"]){
+                        std::string type_str=device_config_node[esc_name]["motor_type"].as<std::string>();
+                        
+                        if(type_str=="ADVRF"){
+                            _ec_cfg.device_config_map[esc_id].type=iit::ecat::CENT_AC;
+                        }else if(type_str=="Synapticon"){
+                            _ec_cfg.device_config_map[esc_id].type=iit::ecat::SYNAPTICON_v5_1;
+                        }else{
+                            _ec_cfg.device_config_map[esc_id].type=iit::ecat::CENT_AC;
+                        }
                     }
+                }
+                else if(device_type=="valve"){
+                    _ec_cfg.device_config_map[esc_id].type=iit::ecat::HYQ_KNEE;
+                }
+                else if(device_type=="pump"){
+                    _ec_cfg.device_config_map[esc_id].type=iit::ecat::HYQ_HPU;
+                }
+                else{
+                    throw std::runtime_error("Error: cannot find a device type for id: "+std::to_string(esc_id));
                 }
 
                 _ec_cfg.device_config_map[esc_id].brake_present=false;
@@ -202,7 +217,7 @@ void EcUtils::config_device()
                     if(config_path_abs!=""){
                         std::cout <<  device_type +" configuration path: " << config_path_abs<< std::endl;
                         auto config_node = YAML::LoadFile(config_path_abs);
-                        device_config_map(config_node);
+                        device_config_map(config_node,device_type);
                     }else{
                         throw std::runtime_error("Wrong "+config_path+" path!");
                     }
@@ -216,15 +231,18 @@ void EcUtils::config_device()
 
 void EcUtils::config_trajectory()
 {
+    _ec_cfg.trj_type="none";
     if(_robot_control_node["trajectory"]["type"]){
         _ec_cfg.trj_type=_robot_control_node["trajectory"]["type"].as<std::string>();
     }
+    _ec_cfg.trj_time=1/(10*_ec_cfg.period_ms);
     if(_robot_control_node["trajectory"]["frequency"]){
         _ec_cfg.trj_time=1/(_robot_control_node["trajectory"]["frequency"].as<float>());
         if(2*_ec_cfg.period_ms> (_ec_cfg.trj_time*1000)){
             throw std::runtime_error("The trajectoy time should be at least two times higher then the period_ms variable");
         }
     }
+    _ec_cfg.repeat_trj=1;
     if(_robot_control_node["trajectory"]["repeat"]){
         _ec_cfg.repeat_trj=_robot_control_node["trajectory"]["repeat"].as<int>();
     }
@@ -235,48 +253,52 @@ void EcUtils::config_trajectory()
                 auto id_vector = _robot_control_node[device_type]["id"].as<std::vector<int>>();
 
                 for(const auto &id: id_vector){
-                    if(_ec_cfg.device_config_map.count(id)==0 && device_type!="pump"){
+                    if(_ec_cfg.device_config_map.count(id)==0){
                         throw std::runtime_error("The ID: " + std::to_string(id) + " hasn't a " +  device_type + " configuration, please setup the control mode");
                     }
                 }
-
-                std::map<std::string,double> set_point;
-                if(_robot_control_node[device_type]["set_point"]){
-                    set_point = _robot_control_node[device_type]["set_point"].as<std::map<std::string,double>>();
-                }
-                else{
-                    throw std::runtime_error("Fatal error: cannot find the set point for the device");
-                }
-
-                std::vector<double> homing,trajectory;
-                if(_robot_control_node[device_type]["homing"]){
-                    homing = _robot_control_node[device_type]["homing"].as<std::vector<double>>();
-                }
-
-                if(_robot_control_node[device_type]["trajectory"]){
-                    trajectory = _robot_control_node[device_type]["trajectory"].as<std::vector<double>>();
-                }
-
-                if(id_vector.size() != homing.size() && !homing.empty()){
-                    throw std::runtime_error("Motor id size has different size of homing vector");
-                }
-                else{
-                    if(homing.size() != trajectory.size() ){
-                        throw std::runtime_error("Homing vector size has different size of trajectory vector");
+                
+                if(_ec_cfg.trj_type!="none"){
+                    std::map<std::string,double> set_point;
+                    if(_robot_control_node[device_type]["set_point"]){
+                        set_point = _robot_control_node[device_type]["set_point"].as<std::map<std::string,double>>();
                     }
-                }
-
-                int i=0;
-                for(const auto &id:id_vector){
-                    if(_ec_cfg.trj_config_map[device_type].set_point.count(id)==0){
-                        _ec_cfg.trj_config_map[device_type].set_point[id]=set_point;
-                        if(!homing.empty() && !trajectory.empty()){
-                            _ec_cfg.trj_config_map[device_type].homing[id]=homing[i];
-                            _ec_cfg.trj_config_map[device_type].trajectory[id]=trajectory[i];
-                            i++;
+                    else{
+                        if(_ec_cfg.trj_type!="polynomial"){
+                            throw std::runtime_error("Fatal error: cannot find the set point for the device");
                         }
-                    }else{
-                        throw std::runtime_error(device_type + " id duplicated!");     
+                    }
+
+                    std::vector<double> homing,trajectory;
+                    if(_robot_control_node[device_type]["homing"]){
+                        homing = _robot_control_node[device_type]["homing"].as<std::vector<double>>();
+                    }
+
+                    if(_robot_control_node[device_type]["trajectory"]){
+                        trajectory = _robot_control_node[device_type]["trajectory"].as<std::vector<double>>();
+                    }
+
+                    if(id_vector.size() != homing.size() && !homing.empty()){
+                        throw std::runtime_error("Motor id size has different size of homing vector");
+                    }
+                    else{
+                        if(homing.size() != trajectory.size() ){
+                            throw std::runtime_error("Homing vector size has different size of trajectory vector");
+                        }
+                    }
+
+                    int i=0;
+                    for(const auto &id:id_vector){
+                        if(_ec_cfg.trj_config_map[device_type].set_point.count(id)==0){
+                            _ec_cfg.trj_config_map[device_type].set_point[id]=set_point;
+                            if(!homing.empty() && !trajectory.empty()){
+                                _ec_cfg.trj_config_map[device_type].homing[id]=homing[i];
+                                _ec_cfg.trj_config_map[device_type].trajectory[id]=trajectory[i];
+                                i++;
+                            }
+                        }else{
+                            throw std::runtime_error(device_type + " id duplicated!");     
+                        }
                     }
                 }
             }
@@ -286,62 +308,61 @@ void EcUtils::config_trajectory()
 
 void EcUtils::trajectory_generator()
 {
+    if ( _ec_cfg.trj_type=="none" || _ec_cfg.trj_type=="polynomial" ){
+        return;
+    }
+
     double trj_time = _ec_cfg.trj_time * _ec_cfg.repeat_trj;
     for(const auto &device_type:_device_type_vector){
         for(const auto &[id,set_poit_map]:_ec_cfg.trj_config_map[device_type].set_point){
             for(const auto &[ctrl_type,set_poit]:set_poit_map){
-                if ( _ec_cfg.trj_type=="polynomial"){
-                    return;
+                // Spline trajectory
+                if ( _ec_cfg.trj_type=="spline"){
+                    std::vector<double> time =  {0,_ec_cfg.trj_time};
+                    std::vector<double> value = {0,set_poit};
+                    _ec_cfg.trj_config_map[device_type].trj_generator[id][ctrl_type] = std::make_shared<Spline_trajectory>(time, value);
+                } 
+                // Smoother trajectory
+                else if ( _ec_cfg.trj_type=="smoother"){
+                    std::vector<double> time =  {0,_ec_cfg.trj_time};
+                    std::vector<double> value = {0,set_poit};
+                    _ec_cfg.trj_config_map[device_type].trj_generator[id][ctrl_type] = std::make_shared<Smoother_trajectory>(time, value);
+                } 
+                // Smooth step
+                else if ( _ec_cfg.trj_type=="step"){
+                    std::vector<double> time =  {0,trj_time};
+                    double amplitude = set_poit;
+                    double freq = 1/_ec_cfg.trj_time;
+                    double theta = 0.0;
+                    _ec_cfg.trj_config_map[device_type].trj_generator[id][ctrl_type] = std::make_shared<Steps_trajectory>(freq, amplitude,theta,time);
+                } 
+                // Sine trajectory
+                else if ( _ec_cfg.trj_type=="sine"){
+                    std::vector<double> time =  {0,trj_time};
+                    double amplitude = set_poit;
+                    double freq = 1/_ec_cfg.trj_time;
+                    double theta = 0.0;
+                    _ec_cfg.trj_config_map[device_type].trj_generator[id][ctrl_type] = std::make_shared<Sine_trajectory>(freq, amplitude, theta, time);
+                } 
+                // Stair trajectory
+                else if ( _ec_cfg.trj_type=="stair"){
+                    double T=0.1*_ec_cfg.trj_time;
+                    double amplitude = T * set_poit;
+                    double max_amplitude = set_poit;
+                    double t_trans = _ec_cfg.trj_time/(1000*_ec_cfg.period_ms);
+                    _ec_cfg.trj_config_map[device_type].trj_generator[id][ctrl_type] = std::make_shared<Stair_trajectory>(T, amplitude, max_amplitude, t_trans);
+                } 
+                // Chirp trajectory
+                else if ( _ec_cfg.trj_type=="chirp"){
+                    double amplitude = set_poit;
+                    double min_f = 1/_ec_cfg.trj_time;
+                    double max_f = 10*min_f;
+                    bool reverse = false;
+                    double step_size = 0.1;
+                    _ec_cfg.trj_config_map[device_type].trj_generator[id][ctrl_type] = std::make_shared<Chirp_trajectory>(amplitude, min_f, max_f,reverse,step_size);
                 }
                 else{
-                    // Spline trajectory
-                    if ( _ec_cfg.trj_type=="spline"){
-                        std::vector<double> time =  {0,_ec_cfg.trj_time};
-                        std::vector<double> value = {0,set_poit};
-                        _ec_cfg.trj_config_map[device_type].trj_generator[id][ctrl_type] = std::make_shared<Spline_trajectory>(time, value);
-                    } 
-                    // Smoother trajectory
-                    else if ( _ec_cfg.trj_type=="smoother"){
-                        std::vector<double> time =  {0,_ec_cfg.trj_time};
-                        std::vector<double> value = {0,set_poit};
-                        _ec_cfg.trj_config_map[device_type].trj_generator[id][ctrl_type] = std::make_shared<Smoother_trajectory>(time, value);
-                    } 
-                    // Smooth step
-                    else if ( _ec_cfg.trj_type=="step"){
-                        std::vector<double> time =  {0,trj_time};
-                        double amplitude = set_poit;
-                        double freq = 1/_ec_cfg.trj_time;
-                        double theta = 0.0;
-                        _ec_cfg.trj_config_map[device_type].trj_generator[id][ctrl_type] = std::make_shared<Steps_trajectory>(freq, amplitude,theta,time);
-                    } 
-                    // Sine trajectory
-                    else if ( _ec_cfg.trj_type=="sine"){
-                        std::vector<double> time =  {0,trj_time};
-                        double amplitude = set_poit;
-                        double freq = 1/_ec_cfg.trj_time;
-                        double theta = 0.0;
-                        _ec_cfg.trj_config_map[device_type].trj_generator[id][ctrl_type] = std::make_shared<Sine_trajectory>(freq, amplitude, theta, time);
-                    } 
-                    // Stair trajectory
-                    else if ( _ec_cfg.trj_type=="stair"){
-                        double T=0.1*_ec_cfg.trj_time;
-                        double amplitude = T * set_poit;
-                        double max_amplitude = set_poit;
-                        double t_trans = _ec_cfg.trj_time/(1000*_ec_cfg.period_ms);
-                        _ec_cfg.trj_config_map[device_type].trj_generator[id][ctrl_type] = std::make_shared<Stair_trajectory>(T, amplitude, max_amplitude, t_trans);
-                    } 
-                    // Chirp trajectory
-                    else if ( _ec_cfg.trj_type=="chirp"){
-                        double amplitude = set_poit;
-                        double min_f = 1/_ec_cfg.trj_time;
-                        double max_f = 10*min_f;
-                        bool reverse = false;
-                        double step_size = 0.1;
-                        _ec_cfg.trj_config_map[device_type].trj_generator[id][ctrl_type] = std::make_shared<Chirp_trajectory>(amplitude, min_f, max_f,reverse,step_size);
-                    }
-                    else{
-                        throw std::runtime_error("Trajectory type not recognized!");
-                    }
+                    throw std::runtime_error("Trajectory type not recognized!");
                 }
             }
         }
@@ -350,31 +371,13 @@ void EcUtils::trajectory_generator()
 
 void EcUtils::generate_fake_slave_info()
 {
-    int slave_pos=1;
     
-    // Motor
-    if(_ec_cfg.trj_config_map.count("motor")>0){
-        for(const auto &[id,set_point]:_ec_cfg.trj_config_map["motor"].set_point){
-            _ec_cfg.fake_slave_info.push_back(std::make_tuple(id,_ec_cfg.device_config_map[id].type,slave_pos));
-            slave_pos++;
-        }
+    int slave_pos=1;
+    for(const auto &[id,device_config]:_ec_cfg.device_config_map){
+        _ec_cfg.fake_slave_info.push_back(std::make_tuple(id,device_config.type,slave_pos));
+        slave_pos++;
     }
 
-    // Valve
-    if(_ec_cfg.trj_config_map.count("valve")>0){
-        for(const auto &[id,set_point]:_ec_cfg.trj_config_map["valve"].set_point){
-            _ec_cfg.fake_slave_info.push_back(std::make_tuple(id,iit::ecat::HYQ_KNEE,slave_pos));
-            slave_pos++;
-        }
-    }
-
-    // Pump
-    if(_ec_cfg.trj_config_map.count("pump")>0){
-        for(const auto &[id,set_point]:_ec_cfg.trj_config_map["pump"].set_point){
-            _ec_cfg.fake_slave_info.push_back(std::make_tuple(id,iit::ecat::HYQ_HPU,slave_pos));
-            slave_pos++;
-        }
-    }
 
     if(_robot_control_node["simulation"]) {
         if(_robot_control_node["simulation"]["imu_id"]){
