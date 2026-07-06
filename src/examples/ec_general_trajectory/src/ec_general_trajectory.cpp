d843b45d8e3b2b2e6f86f27fb0b875c83890ca65
DavideAntonucci <davide.antonucci@iit.it>
2026-07-06 17:17:52 +0200

    Modified also general trajectory example.

--- src/examples/ec_general_trajectory/src/ec_general_trajectory.cpp
+++ src/examples/ec_general_trajectory/src/ec_general_trajectory.cpp
@@ -41,66 +41,19 @@ int main(int argc, char * const argv[])
 
     if(ec_sys_started){                       
         int overruns = 0;
-
-        std::map<int, double> motors_set_zero,motors_set_ref, motors_start;
-        std::map<int, double> valves_set_zero, valves_set_ref, valves_start;
-        std::map<int,Trj_ptr> general_trj;
-
         int trajectory_counter=0;
         float tau=0,alpha=0;
 
-        for (const auto &[esc_id, valve_rx_pdo] : valve_status_map){
-            if(ec_cfg.trj_config_map.count("valve")>0){
-                if(ec_cfg.trj_config_map["valve"].set_point.count(esc_id)>0){
-                    std::string set_point_type="";
-                    if(ec_cfg.device_config_map[esc_id].control_mode_type==iit::advr::Gains_Type_POSITION){
-                        set_point_type="position";
-                        valves_start[esc_id] = std::get<0>(valve_rx_pdo); // actual encoder position
-                    }else if(ec_cfg.device_config_map[esc_id].control_mode_type==iit::advr::Gains_Type_IMPEDANCE){
-                        set_point_type="force";
-                        valves_set_zero[esc_id]=valves_start[esc_id] = 0.0;
-                    }else{
-                        set_point_type="current";
-                        valves_set_zero[esc_id]=valves_start[esc_id] = 0.0;
-                    }
-                    
-                    if(ec_cfg.trj_config_map["valve"].set_point[esc_id].count(set_point_type)>0){
-                        general_trj[esc_id]= ec_cfg.trj_config_map["valve"].trj_generator[esc_id][set_point_type];
-                        valves_set_ref[esc_id]= valves_start[esc_id];
-                    }
-                }
-            }
+        std::map<int,Trj_ptr> general_trj;
+        for (auto &[esc_id, motor_trj] : motor_trj_map){
+            general_trj[esc_id] = motor_trj.general_trj;
+        }
+        for (auto &[esc_id, valve_trj] : valve_trj_map){
+            general_trj[esc_id] = valve_trj.general_trj;
         }
 
-        for (const auto &[esc_id, motor_rx_pdo] : motor_status_map){
-            if(ec_cfg.trj_config_map.count("motor")>0){
-                if(ec_cfg.trj_config_map["motor"].set_point.count(esc_id)>0){
-                    std::string set_point_type="";
-                    if(ec_cfg.device_config_map[esc_id].control_mode_type==iit::advr::Gains_Type_POSITION ||
-                       ec_cfg.device_config_map[esc_id].control_mode_type==iit::advr::Gains_Type_IMPEDANCE){
-                        set_point_type="position";
-                        motors_start[esc_id]=std::get<2>(motor_rx_pdo); // actual motor pos
-                    }else if(ec_cfg.device_config_map[esc_id].control_mode_type==iit::advr::Gains_Type_VELOCITY){
-                        set_point_type="velocity";
-                        motors_set_zero[esc_id]=motors_start[esc_id]=0.0;
-                    }else if(ec_cfg.device_config_map[esc_id].control_mode_type==iit::advr::Gains_Type_TORQUE){
-                        set_point_type="torque";
-                        motors_set_zero[esc_id]=motors_start[esc_id]=0.0;
-                    }else{
-                        set_point_type="current";
-                        motors_set_zero[esc_id]=motors_start[esc_id]=0.0;
-                    }
-
-                    if(ec_cfg.trj_config_map["motor"].trj_generator[esc_id].count(set_point_type)>0){
-                        general_trj[esc_id]= ec_cfg.trj_config_map["motor"].trj_generator[esc_id][set_point_type];
-                        motors_set_ref[esc_id]= motors_start[esc_id];
-                    }
-                }
-            }
-        }
-
-        if(motors_set_ref.empty() && valves_set_ref.empty()){
-            fatal_error="fatal error: motor references and valves references are both empty";
+        if(general_trj.empty()){
+            fatal_error="fatal error: general trajectory map empty";
             run_loop=false;
         }else{
             if (ec_cfg.protocol == "iddp"){
@@ -150,7 +103,7 @@ int main(int argc, char * const argv[])
                 int ctrl_mode= ec_cfg.device_config_map[esc_id].control_mode_type;
                 if(valve_reference_map.count(esc_id)>0){
                     if(!run_loop){
-                        target=valves_set_zero[esc_id];
+                        target=valve_trj_map[esc_id].set_zero;
                         if(target == iit::advr::Gains_Type_POSITION){
                             client->get_valve_status(valve_status_map);
                             target=std::get<0>(valve_status_map[esc_id]); // actual encoder position
@@ -166,7 +119,7 @@ int main(int argc, char * const argv[])
                 }
                 else if(motor_reference_map.count(esc_id)>0){
                     if(!run_loop){
-                        target=motors_set_zero[esc_id];
+                        target=motor_trj_map[esc_id].set_zero;
                         if(ctrl_mode == iit::advr::Gains_Type_POSITION ||
                             ctrl_mode == iit::advr::Gains_Type_IMPEDANCE){
                             client->get_motor_status(motor_status_map);
